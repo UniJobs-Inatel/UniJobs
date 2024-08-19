5768b547d27552d6e2d7e28854d159396316ad49
Maria Luiza <malusraimundo@gmail.com>
2024-08-19 14:44:06 -0300

    settings

--- client-web/vite.config.ts
+++ client-web/vite.config.ts
@@ -1,13 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    alias: { 
+      "@": path.resolve(__dirname, "./src/"),
       "@pages": path.resolve(__dirname, "./src/pages/"),
-    },
+     },
   },
-})
+});
