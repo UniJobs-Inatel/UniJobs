a7ab99abd72b4577bd055f8a31471f714d56ee6d
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-09-07 13:48:43 -0300

    Student profile route

---
+++ server/src/student/dto.ts
@@ -0,0 +1,71 @@
+export class CreateStudentProfileDto {
+  student: {
+    first_name: string;
+    last_name: string;
+    cpf: string;
+  };
+
+  experiences: {
+    type: 'professional' | 'academic';
+    description: string;
+    company_name: string;
+    position: string;
+    start_date: Date;
+    end_date: Date | null;
+  }[];
+
+  proficiencies: {
+    id: number;
+  }[];
+}
+
+export class UpdateStudentProfileDto {
+  student: {
+    first_name: string;
+    last_name: string;
+    cpf: string;
+  };
+
+  experiences: {
+    id?: number;
+    type: 'professional' | 'academic';
+    description: string;
+    company_name: string;
+    position: string;
+    start_date: Date;
+    end_date: Date | null;
+  }[];
+
+  proficiencies: {
+    id: number;
+  }[];
+}
+
+export class CreateExperienceDto {
+  studentId: number;
+  type: 'professional' | 'academic';
+  description: string;
+  company_name: string;
+  position: string;
+  start_date: Date;
+  end_date: Date | null;
+}
+
+export class UpdateExperienceDto {
+  type?: 'professional' | 'academic';
+  description?: string;
+  company_name?: string;
+  position?: string;
+  start_date?: Date;
+  end_date?: Date | null;
+}
+
+export class ListExperienceDto {
+  id: number;
+  type: 'professional' | 'academic';
+  description: string;
+  company_name: string;
+  position: string;
+  start_date: Date;
+  end_date: Date | null;
+}
---
+++ server/src/student/student.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Put,
+  Get,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { StudentService } from './student.service';
+import {
+  CreateStudentProfileDto,
+  UpdateStudentProfileDto,
+  CreateExperienceDto,
+  UpdateExperienceDto,
+} from './dto';
+
+@Controller('student')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+
+  @Post('profile')
+  @HttpCode(HttpStatus.CREATED)
+  async createProfile(
+    @Body() createStudentProfileDto: CreateStudentProfileDto,
+  ) {
+    return this.studentService.createProfile(createStudentProfileDto);
+  }
+
+  @Put('profile/:id')
+  async updateProfile(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateStudentProfileDto: UpdateStudentProfileDto,
+  ) {
+    return this.studentService.updateProfile(id, updateStudentProfileDto);
+  }
+
+  @Post('experience')
+  @HttpCode(HttpStatus.CREATED)
+  async createExperience(@Body() createExperienceDto: CreateExperienceDto) {
+    return this.studentService.createExperience(createExperienceDto);
+  }
+
+  @Get('experience')
+  async getAllExperiences() {
+    return this.studentService.getAllExperiences();
+  }
+
+  @Get('experience/:id')
+  async getExperienceById(@Param('id', ParseIntPipe) id: number) {
+    return this.studentService.getExperienceById(id);
+  }
+
+  @Put('experience/:id')
+  async updateExperience(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateExperienceDto: UpdateExperienceDto,
+  ) {
+    return this.studentService.updateExperience(id, updateExperienceDto);
+  }
+
+  @Delete('experience/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteExperience(@Param('id', ParseIntPipe) id: number) {
+    await this.studentService.deleteExperience(id);
+  }
+}
