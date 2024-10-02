1cc786dbb4480b55bee728b8a5baf9d1b85f1a7a
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-10-02 19:32:52 -0300

    Hashing password on User CRUD

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -11,6 +16,18 @@ export class UserService {
   ) {}
 
   async create(userData: Partial<User>): Promise<User> {
+    const existingUser = await this.userRepository.findOne({
+      where: { email: userData.email },
+    });
+    if (existingUser) {
+      throw new ConflictException('Email already registered.');
+    }
+
+    if (userData.password) {
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+      userData.password = hashedPassword;
+    }
+
     const user = this.userRepository.create(userData);
     return this.userRepository.save(user);
   }
@@ -28,14 +45,18 @@ export class UserService {
   }
 
   async update(id: number, userData: Partial<User>): Promise<User> {
-    const user = await this.userRepository.preload({
-      id: +id,
-      ...userData,
-    });
+    const user = await this.findOne(id);
     if (!user) {
       throw new NotFoundException(`User with ID ${id} not found`);
     }
-    return this.userRepository.save(user);
+
+    if (userData.password) {
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+      userData.password = hashedPassword; // Hash the new password
+    }
+
+    const updatedUser = this.userRepository.merge(user, userData);
+    return this.userRepository.save(updatedUser);
   }
 
   async remove(id: number): Promise<void> {
