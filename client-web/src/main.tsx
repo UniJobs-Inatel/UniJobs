e0d523c0c08c4999b78dade4339bb0e9788bc15c
Manuela Gripp <manugrippsilva@hotmail.com>
2024-10-30 22:46:33 -0300

    feat: feedback modal

--- client-web/src/main.tsx
+++ client-web/src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
+import Modals from "./components/ui/modal";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App/>
+    <Modals />
   </React.StrictMode>
 );
