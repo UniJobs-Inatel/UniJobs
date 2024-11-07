378aa6936276a23b6edbb28581ebc6a85096a8e0
Manuela Gripp <manugrippsilva@hotmail.com>
2024-11-06 21:43:35 -0300

    feat: allowed route

--- client-web/src/router/privateRoute.tsx
+++ client-web/src/router/privateRoute.tsx
@@ -1,9 +1,17 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import useAuthStore from "@/stores/authStore";
 import { UserStatus, UserType } from "@/domain/user";
 
+
+const allowedRoutes: { [key in UserType]: string[] } = {
+  [UserType.COLLEGE]: ["/perfil-empresa", "/vagas-publicadas", "/cadastrar-vaga",],
+  [UserType.COMPANY]: ["/perfil-empresa", "/vagas-publicadas", "/cadastrar-vaga",],
+  [UserType.STUDENT]: ["/perfil-estudante",  "/vagas", "/vagas-favoritadas"],
+};
+
 const PrivateRoute = () => {
   const { user } = useAuthStore.getState();
+  const location = useLocation();
 
   if (!user) {
     return <Navigate to="/" />;
@@ -11,6 +19,11 @@ const PrivateRoute = () => {
 
   if (user.status != UserStatus.COMPLETE) {
      <Navigate to={user.type == UserType.COLLEGE ? '/perfil-estudante' : '/perfil-empresa' } />;
+  }
+
+  const isRouteAllowed = allowedRoutes[user.type]?.includes(location.pathname);
+  if (!isRouteAllowed) {
+    return <Navigate to="/perfil-empresa" />; // Redireciona para uma página de acesso negado
   }
 
   return <Outlet />;
