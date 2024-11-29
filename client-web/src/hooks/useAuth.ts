d880fe3732b15270ea691324f81febc00bc10a81
Manuela Gripp <manugrippsilva@hotmail.com>
2024-11-29 15:21:28 -0300

    feat: add env in url refresh token

--- client-web/src/hooks/useAuth.ts
+++ client-web/src/hooks/useAuth.ts
@@ -10,7 +10,7 @@ const useAuth = () => {
       if (!refreshToken) return;
 
       try {
-        const response = await axios.post('http://localhost:4000/api/auth/refresh-token', {
+        const response = await axios.post(`${import.meta.env.VITE_API_URL as string}auth/refresh-token`, {
           refreshToken,
         });
         const { accessToken: newAccessToken, refreshToken: newRefreshToken } = response.data;
