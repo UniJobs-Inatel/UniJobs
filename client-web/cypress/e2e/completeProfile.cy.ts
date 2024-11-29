74a673f4422a8394b8fd165198ac80d825970139
Manuela Gripp <manugrippsilva@hotmail.com>
2024-11-29 15:16:36 -0300

    test: complete profile

--- client-web/cypress/e2e/completeProfile.cy.ts
+++ client-web/cypress/e2e/completeProfile.cy.ts
@@ -22,46 +22,43 @@ describe('CompanyProfile', () => {
 
 });
 
-// describe('StudentProfile', () => {
+describe('StudentProfile', () => {
   
-//   it('it should complete a company profile', () => {
+  it('it should complete a company profile', () => {
     
-//     login('studentCy@yopmail.com', 'Senh@123')
+    login('studentCy@yopmail.com', 'Senh@123')
     
-//     cy.url().should('include', '/perfil-estudante');
+    cy.url().should('include', '/perfil-estudante');
 
-//     cy.get('input[name=first_name]').type('Jeniffer');
-//     cy.get('input[name=last_name]').type('Souza');
-//     cy.get('input[name=cpf]').type('19609012060');
+    cy.get('input[name=first_name]').type('Jeniffer');
+    cy.get('input[name=last_name]').type('Souza');
+    cy.get('input[name=cpf]').type('19609012060');
     
-//     cy.get('[data-cy="multiSelect"]').click()
+    cy.get('[data-cy="multiSelect"]').click()
     
-//     cy.get('[data-cy="multiSelect-item"]').first().click()
+    cy.get('[data-cy="multiSelect-item"]').first().click()
     
-//     cy.get('[data-cy="multiSelect"]').click()
+    cy.get('[data-cy="multiSelect"]').click()
     
-//     cy.contains('Adicionar Experiência').click()
+    cy.contains('Adicionar Experiência').click()
     
-//     cy.get('input[value=professional]').click()
-//     cy.get('input[name=position]').type('Especialista em Marketing Digital');
-//     cy.get('input[name=company_name]').type('DigitalExpand');
-//     cy.get('input[name=start_date]').type('10/06/2021');
-//     cy.get('input[name=end_date]').type('01/11/2022');
-//     cy.get('textarea[name=description]').type('Planejamento e execução de estratégias de SEO e campanhas de anúncios pagos.');
+    cy.get('input[value=professional]').click()
+    cy.get('input[name=position]').type('Especialista em Marketing Digital');
+    cy.get('input[name=company_name]').type('DigitalExpand');
+    cy.get('input[name=start_date]').type('10/06/2021');
+    cy.get('input[name=end_date]').type('01/11/2022');
+    cy.get('textarea[name=description]').type('Planejamento e execução de estratégias de SEO e campanhas de anúncios pagos.');
     
-//     cy.get('[data-cy=experience-button]').click();
+    cy.get('[data-cy=experience-button]').click();
     
-//     cy.get('[data-cy="experiences"]').first().within(() => {
-//       cy.get('h3')
-//         .should('be.visible')
-//         .and('contain.text', 'Especialista em Marketing Digital'); 
-//     });
+    cy.get('[data-cy="experiences"]').first().within(() => {
+      cy.get('h3')
+        .should('be.visible')
+        .and('contain.text', 'Especialista em Marketing Digital'); 
+    });
 
-//     cy.get('button').contains('Salvar').click()
+    cy.get('button').contains('Salvar').click()
     
-//     cy.url().should('include', '/vagas');
+  });
 
-    
-//   });
-
-// })
+})
